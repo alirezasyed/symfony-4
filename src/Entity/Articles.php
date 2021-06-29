ba1efbb413e4aa8902189484b3795a8edda70f11
alirezasyed <codingchallenge2020@yahoo.com>
2021-06-29 06:17:44 -0700

    automatise les created at et updated at avec l'extension stof Gedmo

--- src/Entity/Articles.php
+++ src/Entity/Articles.php
@@ -37,11 +37,17 @@ class Articles
     private $contenu;
 
     /**
+     * @var \DateTime $created_at
+     * 
+     * @Gedmo\Timestampable(on="create")
      * @ORM\Column(type="datetime")
      */
     private $created_at;
 
     /**
+     * @var \DateTime $updated_at
+     * 
+     * @Gedmo\Timestampable(on="update")
      * @ORM\Column(type="datetime")
      */
     private $updated_at;
@@ -118,23 +124,9 @@ class Articles
         return $this->created_at;
     }
 
-    public function setCreatedAt(\DateTimeInterface $created_at): self
-    {
-        $this->created_at = $created_at;
-
-        return $this;
-    }
-
     public function getUpdatedAt(): ?\DateTimeInterface
     {
         return $this->updated_at;
-    }
-
-    public function setUpdatedAt(\DateTimeInterface $updated_at): self
-    {
-        $this->updated_at = $updated_at;
-
-        return $this;
     }
 
     public function getFeaturedImage(): ?string
